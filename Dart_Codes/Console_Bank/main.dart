b45856e3bdc201af0561eb87173d44f222bfb347
kadelcode <kadelcode@gmail.com>
2022-10-17 22:58:40 +0100

    Adding the Bank class

--- Dart_Codes/Console_Bank/main.dart
+++ Dart_Codes/Console_Bank/main.dart
@@ -1,6 +1,16 @@
 class Bank
 {
-  // Declaring field names
+  String acc_name= ""; // account name
+  int acc_num = 0; // account number
+  int acc_bal = 0; // account balance
+  // using the constructor function to initialize variables
+   Bank(String acc_name, int acc_num, int acc_bal)
+  {
+    this.acc_name = acc_name;
+    this.acc_num = acc_num;
+    this.acc_bal = acc_bal;
+
+  }
   
   // class methods
 }
